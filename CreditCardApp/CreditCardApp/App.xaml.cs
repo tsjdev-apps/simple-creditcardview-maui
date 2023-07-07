b3e0265f34340f3fce32d4b6d5d1444a7f7d0976
Sebastian Jensen <apps@tsjdev-apps.de>
2023-07-07 20:52:28 +0200

    Add implementation of CreditCardView in .NET MAUI.

---
+++ CreditCardApp/CreditCardApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CreditCardApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CreditCardApp/CreditCardApp/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CreditCardApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("FontAwesome6-Brands.otf", "FA6Brands");
+                fonts.AddFont("FontAwesome6-Regular.otf", "FA6Regular");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
